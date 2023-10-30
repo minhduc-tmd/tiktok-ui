0d74f49e60878117f199d662fe123736e46d170a
minhduc <minhduc.ptit.95@gmail.com>
2023-10-30 17:34:35 +0700

    update 30/10/2023

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-// import { useState } from "react";
-// import Content from "./Content";
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
 
 function App() {
-  return (
-    <div className="App">
-      <nav>
-        <ul>
-          <li>
-            <a href="/" onClick={''}>Home</a>
-          </li>
-          <li>
-            <a href="/news" onClick={''}>News</a>
-          </li>
-          <li>
-            <a href="/contact" onClick={''}>Contact</a>
-          </li>
-        </ul>
-      </nav>
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
 
-      <h1>News Page</h1>
-    </div>
-  );
+                        let Layout = DefaultLayout;
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/pages/Search/index.js
@@ -0,0 +1,5 @@
+function Search() {
+    return <h2> Home</h2>;
+}
+
+export default Search;
